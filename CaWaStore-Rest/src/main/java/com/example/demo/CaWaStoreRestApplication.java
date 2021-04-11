7198af81f6148e4f2ae527a2bb5322596127c844
joseangel <ja.sanchezp.2017@alumnos.urjc.es>
2021-04-11 17:02:13 +0200

    Commit recibido

--- CaWaStore-Rest/src/main/java/com/example/demo/CaWaStoreRestApplication.java
+++ CaWaStore-Rest/src/main/java/com/example/demo/CaWaStoreRestApplication.java
@@ -8,7 +8,7 @@ public class CaWaStoreRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CaWaStoreRestApplication.class, args);
-		System.out.println("El commit ha funcionado");
+		System.out.println("Recibido");
 	}
 
 }
